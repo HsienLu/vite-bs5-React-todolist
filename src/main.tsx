be635c7510d3f38a7743d7a1e9bf5eaf754d5681
HsienLu <sjps946403@gmail.com>
2024-02-23 00:50:43 +0800

    20240223

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import Login from "./component/login.tsx";
+import Login from "./component/Login.tsx";
 import {HashRouter, Route, Routes} from "react-router-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import TodoList from "./component/TodoList.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/TodoList" element={<TodoList />} />
       </Routes>
     </HashRouter>
   </React.StrictMode>
